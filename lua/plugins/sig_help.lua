a49653afff0b8f1a308b6f13ba0a6d2651d568dc
Atul Mane <maneatul225@gmail.com>
2025-10-26 18:24:17 +0530

    lsp signature help

---
+++ lua/plugins/sig_help.lua
@@ -0,0 +1,15 @@
+return {
+	"ray-x/lsp_signature.nvim",
+	event = "InsertEnter",
+	opts = {
+		-- cfg options
+	},
+	opts = {
+		bind = true,
+		handler_opts = { border = "rounded" },
+		hint_enable = true,
+		hint_prefix = "💡 ",
+		floating_window = true,
+		floating_window_above_cur_line = true,
+	},
+}
